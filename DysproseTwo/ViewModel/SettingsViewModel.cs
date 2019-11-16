088bc2d4dd2ce9322141a5d38d9aec2ca33d56e8
Colin Kiama <colinkiama@gmail.com>
2019-11-16 14:59:03 +0000

    TimeUnits now show up in combo box

--- DysproseTwo/ViewModel/SettingsViewModel.cs
+++ DysproseTwo/ViewModel/SettingsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DysproseTwo.Enums;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -10,6 +11,22 @@ namespace DysproseTwo.ViewModel
     class SettingsViewModel : Notifier
     {
 
+        private TimeUnit _selectedTimeUnit;
+
+        public TimeUnit SelectedTimeUnit
+        {
+            get { return _selectedTimeUnit; }
+            set
+            {
+                if (_selectedTimeUnit != value)
+                {
+                    _selectedTimeUnit = value;
+                    NotifyPropertyChanged();
+                }
+
+            }
+        }
+
 
         private double _fontSize;
 
@@ -20,7 +37,7 @@ namespace DysproseTwo.ViewModel
             {
                 if (_fontSize != value)
                 {
-                   _fontSize = value;
+                    _fontSize = value;
                     NotifyPropertyChanged();
                 }
             }
@@ -29,6 +46,11 @@ namespace DysproseTwo.ViewModel
         public List<double> FontList { get; set; } = new List<double>()
         {
             8,9,10,11,12,14,15,18,24,30,36,48,60,72,96
+        };
+
+        public List<TimeUnit> TimeUnits { get; set; } = new List<TimeUnit>()
+        {
+            TimeUnit.Seconds, TimeUnit.Minutes, TimeUnit.Hours
         };
     }
 }
