c1186700b62e6871904db437b2c31089fa8b0dbc
Colin Kiama <colinkiama@gmail.com>
2019-11-16 17:37:43 +0000

    Added structs for use with settings

---
+++ DysproseTwo/Services/SettingsService.cs
@@ -0,0 +1,23 @@
+﻿using DysproseTwo.Structs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DysproseTwo.Services
+{
+    public static class SettingsService
+    {
+        public static void UpdateSessionSettings(DysproseSessionSettings sessionSettings)
+        {
+            
+        }
+
+        public static void UpdateGlobalSettings(DysproseGlobalSettings globalSessionSettings)
+        {
+
+        }
+        public static event EventHandler SettingsUpdated;
+    }
+}
---
+++ DysproseTwo/Structs/DysproseGlobalSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DysproseTwo.Structs
+{
+    public struct DysproseGlobalSettings : IEquatable<DysproseGlobalSettings>
+    {
+        private double fontSize;
+
+        public double FontSize { get => fontSize; set => fontSize = value; }
+
+        public override bool Equals(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator ==(DysproseGlobalSettings left, DysproseGlobalSettings right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(DysproseGlobalSettings left, DysproseGlobalSettings right)
+        {
+            return !(left == right);
+        }
+
+        public bool Equals(DysproseGlobalSettings other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
