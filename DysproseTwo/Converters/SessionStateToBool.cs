f3097d62fa2d03b736532bd8912bd2b6aadfa65e
Colin Kiama <colinkiama@gmail.com>
2019-11-17 03:03:13 +0000

    Now textbox is only enabled when a session is in progress

---
+++ DysproseTwo/Converters/SessionStateToBool.cs
@@ -0,0 +1,34 @@
+﻿using DysproseTwo.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace DysproseTwo.Converters
+{
+    class SessionStateToBool:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool isEnabled = false;
+            if (value is DysproseSessionState sessionState)
+            {
+                switch (sessionState)
+                {
+                    case DysproseSessionState.InProgress:
+                        isEnabled = true;
+                        break;
+                }
+            }
+
+            return isEnabled;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
