9cc21d9966518739c9f97795ae6ab6ea6de34c8b
Colin Kiama <colinkiama@hotmail.co.uk>
2020-12-30 18:20:55 +0000

    feat: Formatted time left tooltip

---
+++ DysproseTwo/Converters/SessionTimeToString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace DysproseTwo.Converters
+{
+    class SessionTimeToString:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string formatString;
+            if (value is TimeSpan sessionTime)
+            {
+                if (sessionTime.Hours > 0)
+                {
+                    formatString = @"hh\:mm\:ss";
+                }
+                else
+                {
+                    formatString = @"mm\:ss";
+                }
+            }
+            else
+            {
+                formatString = "No Time Set";
+            }
+            return sessionTime.ToString(formatString);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
